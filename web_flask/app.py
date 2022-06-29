73c2139533da4b51a009979cf66d9d651b1b2aff
Leul Yiheyis <leulykpro@gmail.com>
2022-06-29 22:03:51 +0300

    intial setup of flask app

---
+++ web_flask/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from models import storage
+from models.user import User
+import bcrypt
+
+
+app = Flask(__name__)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        user_name = request.form['user_name']
+        password = request.form['password']
+        usr = User()
+        usr.first_name = first_name
+        usr.last_name = last_name
+        usr.user_name = user_name
+        usr.password = bcrypt.hashpw(password.encode('utf-8'),
+                                     bcrypt.gensalt())
+        storage.new(usr)
+        storage.save()
+    return render_template('signup.html')
+
+
+@app.route('/signin', methods=['GET', ['POST']])
+def signin():
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        password = request.form['password']
+        hashed_password = ""    # to be queried from database
+        if not bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+            flash('Incorrect password')
+        else:
+            redirect(url_for('index'))
+    return render_template('signin.html')
