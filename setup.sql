5cee2784a03cf9631d61231993fb4d6b8b59c211
Leul Yiheyis <leulykpro@gmail.com>
2022-07-02 10:44:29 +0300

    fix error

--- setup.sql
+++ setup.sql
@@ -2,4 +2,4 @@
 
 CREATE DATABASE IF NOT EXISTS senterezh_db;
 CREATE USER IF NOT EXISTS 'senterezh_dev'@'localhost' IDENTIFIED BY 'senterezh_dev_pwd';
-GRANT ALL PRIVILEGES ON senterzh_db.* TO 'senterezh_dev'@'localhost';
+GRANT ALL PRIVILEGES ON senterezh_db.* TO 'senterezh_dev'@'localhost';
